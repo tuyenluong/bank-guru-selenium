31aa188a609515403b5aab42205b8e1159ddc115
Jeremy <atuyenluong@gmail.com>
2022-06-28 02:07:29 +0700

    Add new instance on PageGenerator

--- src/main/java/Actions/PageGeneratorManager.java
+++ src/main/java/Actions/PageGeneratorManager.java
@@ -2,16 +2,17 @@ package Actions;
 
 import PageObjects.HomePO;
 import PageObjects.LoginPO;
+import PageObjects.NewCustomerPO;
 import org.openqa.selenium.WebDriver;
 
 public class PageGeneratorManager {
-    public static LoginPO getLoginPage(WebDriver driver){
+    protected static LoginPO getLoginPage(WebDriver driver){
         return new LoginPO(driver);
     }
 
-    public static HomePO getHomePage(WebDriver driver){
+    protected static HomePO getHomePage(WebDriver driver){
         return new HomePO(driver);
     }
 
-
+    protected static NewCustomerPO getNewCustomerPage(WebDriver driver){ return new NewCustomerPO(driver);}
 }
